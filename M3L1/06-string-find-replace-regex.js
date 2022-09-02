a750c98ec911e31e5d4b59f0348cfbe881912e03
Martin <martin_lenart@icloud.com>
2022-09-02 10:41:41 +0200

    update

--- M3L1/06-string-find-replace-regex.js
+++ M3L1/06-string-find-replace-regex.js
@@ -6,13 +6,14 @@
 //Just to ensure we force js into strict mode in HTML scrips - we don't want any sloppy code
 'use strict';
 
-const searchString = "Now is the time and this is the time and that is the time";
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/matchAll
+const searchString = "Now is the time aateaa and this is the time and that is the time";
 
 // In javascript a RegEx literal is embarced by //
 // You can see some patterns and test at https://regexr.com
 
 // below RegEx matches word that starts with 't' followed by any word character (0 or more times) and ends with 'e;
-const regexSearch = /t\w*e/g;       
+const regexSearch = /\bt\w{2}e/g;   
 
 const matches = searchString.matchAll(regexSearch);
 for (const match of matches) {
@@ -21,10 +22,11 @@ for (const match of matches) {
 
 
 // Use a regular expression to replace patterns
-const originalString = 'Now is the time, this is the tame';
+const originalString = 'Now is the timesoso, this is the tame';
 
 // below RegEx matches word that starts with 't' followed by any word character (exactly 2 times) and ends with 'e;
-const regexReplace = /t\w{2}e/g;
+const regexReplace = /\bt\w{2}e\b/g;
+
 const newString = originalString.replaceAll(regexReplace, 'place');
 console.log(`The regex ${regexReplace} changes "${originalString}" to "${newString}"`);
 
@@ -36,6 +38,7 @@ console.log(`The regex ${regexReplace} changes "${originalString}" to "${newStri
    Ut enim ad minim <firstName/>, quis nostrud exercitation ullamco <firstName/> nisi ut aliquip ex ea commodo consequat.'
 
 2. Write code that finds all the numbers in below text. Write to console how many numbers there are.
+
  'Lorem3 ipsum 45 dolor sit amet, 5consectetur adipisicing_99 elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
  Ut enim ad minim ven3iam, quis nostrud exercitation ull_88_amco laboris nisi ut aliquip ex ea com36modo 183 consequat.'
 */
